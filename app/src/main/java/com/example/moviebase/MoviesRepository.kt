16ad4f7d2faad0adb44b16c9bb4bb1eaa32c85a9
k3xs <kexs888@ya.ru>
2021-10-11 13:06:49 +0500

    Добавлены зависимости: Room, Coroutines Полученные данные можно посмотреть в Logcat (Verbose - Repository)

---
+++ app/src/main/java/com/example/moviebase/MoviesRepository.kt
@@ -0,0 +1,47 @@
+package com.example.moviebase
+
+
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object MoviesRepository {
+    private val api: TMDBApi
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        api = retrofit.create(TMDBApi::class.java)
+    }
+
+    fun getPopularMovies(page: Int =1 ) {
+        api.getPopularMovies(page = page)
+            .enqueue(object : Callback<MoviesResponse> {
+                override fun onResponse(
+                    call: Call<MoviesResponse>,
+                    response: Response<MoviesResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        val responseBody = response.body()
+
+                        if (responseBody != null) {
+                            Log.d("Repository", "Movies: ${responseBody.movies}")
+                        } else {
+                            Log.d("Repository", "Failed to get response")
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<MoviesResponse>, t: Throwable) {
+                    Log.e("Repository", "onFailure", t)
+                }
+            })
+    }
+
+}
---
+++ app/src/main/java/com/example/moviebase/TMDBApi.kt
@@ -0,0 +1,18 @@
+package com.example.moviebase
+
+
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TMDBApi {
+
+    @GET("movie/popular")
+    fun getPopularMovies(
+        @Query("api_key")
+        apiKey: String = "62c10acc64aa3bd06c3ee9ac146ae91a",
+        @Query("page")
+        page: Int
+    ): Call<MoviesResponse>
+}
