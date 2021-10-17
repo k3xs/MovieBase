87956db2ee3077e82b92709ed962028a74aebe65
k3xs <kexs888@ya.ru>
2021-10-17 23:36:28 +0500

    Выводится список популярных фильмов(1 страница 20 шт.)

--- app/src/main/java/com/example/moviebase/MoviesRepository.kt
+++ app/src/main/java/com/example/moviebase/MoviesRepository.kt
@@ -20,7 +20,11 @@ object MoviesRepository {
         api = retrofit.create(TMDBApi::class.java)
     }
 
-    fun getPopularMovies(page: Int =1 ) {
+    fun getPopularMovies(
+        page: Int = 1,
+        onSuccess: (movies: List<Movie>) -> Unit,
+        onError: () -> Unit
+    ) {
         api.getPopularMovies(page = page)
             .enqueue(object : Callback<MoviesResponse> {
                 override fun onResponse(
@@ -31,15 +35,15 @@ object MoviesRepository {
                         val responseBody = response.body()
 
                         if (responseBody != null) {
-                            Log.d("Repository", "Movies: ${responseBody.movies}")
+                            onSuccess.invoke(responseBody.movies)
                         } else {
-                            Log.d("Repository", "Failed to get response")
+                            onError.invoke()
                         }
                     }
                 }
 
                 override fun onFailure(call: Call<MoviesResponse>, t: Throwable) {
-                    Log.e("Repository", "onFailure", t)
+                    onError.invoke()
                 }
             })
     }
