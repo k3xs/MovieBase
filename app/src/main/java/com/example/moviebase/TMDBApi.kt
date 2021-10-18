8fb1ae181d74e30eab339a9e269398b8e2b6afd2
k3xs <kexs888@ya.ru>
2021-10-18 13:53:19 +0500

    В activity_main.xml подготовлена верстка для вывода самых рейтинговых фильмов.

--- app/src/main/java/com/example/moviebase/TMDBApi.kt
+++ app/src/main/java/com/example/moviebase/TMDBApi.kt
@@ -15,4 +15,13 @@ interface TMDBApi {
         @Query("page")
         page: Int
     ): Call<MoviesResponse>
+
+
+    @GET("movie/top_rated")
+    fun getTopRatedMovies(
+        @Query("api_key")
+        apiKey: String = "62c10acc64aa3bd06c3ee9ac146ae91a",
+        @Query("page")
+        page: Int
+    ): Call<MoviesResponse>
 }
